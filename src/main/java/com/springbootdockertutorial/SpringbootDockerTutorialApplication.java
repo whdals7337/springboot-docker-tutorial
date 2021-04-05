465d3785448067eafa24a4a409d9af923ba6f7d9
wellbell <uok02018496@naver.com>
2021-04-05 23:04:33 +0900

    first commit

---
+++ src/main/java/com/springbootdockertutorial/SpringbootDockerTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdockertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdockertutorial/SpringbootDockerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdockertutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
